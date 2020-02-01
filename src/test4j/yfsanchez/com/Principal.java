fda0bb9e823b381ffbba3079cf1f8a7c65c30d61
yfigueroa <yfsanchez@gmail.com>
2020-02-01 15:15:11 -0300

    agregando log4j.xml
    se agrega configuracion de log4j.xml

---
+++ src/test4j/yfsanchez/com/Principal.java
@@ -0,0 +1,20 @@
+package test4j.yfsanchez.com;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+import org.apache.log4j.xml.DOMConfigurator;
+
+public class Principal {
+
+	static final Logger logger = Logger.getLogger(Principal.class);
+	 
+    public static void main(String[] args)
+    {
+    	//DOMConfigurator is used to configure logger from xml configuration file
+        DOMConfigurator.configure("log4j.xml");
+ 
+        //Log in console in and log file
+        logger.debug("Log4j appender configuration is successful !!");
+    }
+
+}
